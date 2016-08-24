151e12a3b9c1bff6f11fff5227fbdf86de0c4606
priscillamc <priscillamc@users.noreply.github.com>
2016-08-23 21:15:36 -0400

    Updates support for ufclas-syllabus plugin to match version 1.1.0

---
+++ ufclas-syllabus/archive-syllabus.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template file for archives.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package UFCLAS_UFL_2015
+ */
+get_header(); ?>
+
+<div id="main" class="container">
+<div class="row">
+  <div class="col-sm-12">
+    <?php ufclas_ufl_2015_breadcrumbs(); ?>
+    <header class="entry-header">
+      <?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+    </header>
+    <!-- .entry-header --> 
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
+		<ul class="">
+		<?php 
+		while ( have_posts() ) : the_post(); ?>
+            <?php the_title(sprintf( '<li><a href="%s">', get_the_permalink() ), '</a></li>'); ?>
+		<?php 
+		endwhile; // End of the loop. 
+		?>
+    	</ul>
+  </div>
+</div>
+</div>
+
+<?php get_footer(); ?>
