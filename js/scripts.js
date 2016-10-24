0fe42a36f68e24e5a1ea0a66e839df3a6c11fe21
priscillamc <priscillamc@users.noreply.github.com>
2016-10-24 17:07:48 -0400

    Adds smooth scrolling to internal links

--- js/scripts.js
+++ js/scripts.js
@@ -44,6 +44,21 @@ jQuery(function($){
 	dropdownColumns();
 	$(window).resize(dropdownColumns);
 	
+	// Performs a smooth page scroll to an anchor on the same page.
+	// https://css-tricks.com/snippets/jquery/smooth-scrolling/
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+				  scrollTop: target.offset().top - 160
+				}, 500);
+				return false;
+			}
+		}
+	});
+	
 });
 
 // Smartresize
