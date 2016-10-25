699c83e17baae6c9e287b62a3cd68c4060eb956b
priscillamc <priscillamc@users.noreply.github.com>
2016-10-25 19:27:30 -0400

    Updates and adds landing page templates

--- page-templates/landing-page-full-width.php
+++ page-templates/landing-page-full-width.php
@@ -1,20 +1,23 @@
 <?php
 /**
- * Template Name: Landing Page
+ * Template Name: Landing Page (Full-Width)
  * 
  * @package UFCLAS_UFL_2015
  *
  */
 get_header(); ?>
+
 <?php 
-	if ( has_post_thumbnail() ):
-		$shortcode = sprintf( '[ufclas-landing-page-hero-full image="%d" image_height="half"][/ufclas-landing-page-hero-full]', 
-			get_post_thumbnail_id()
-		);
-		echo do_shortcode( $shortcode );
-	endif;
+if ( has_post_thumbnail() ):
+	$shortcode = sprintf( '[ufclas-landing-page-hero-full headline="%s" image="%d" image_height="half"][/ufclas-landing-page-hero-full]', 
+		get_the_title(),
+		get_post_thumbnail_id()
+	);
+	echo do_shortcode( $shortcode );
+endif; 
 ?>
-<div id="main" class="container">
+
+<div id="main">
     <div class="row">
         <div class="col-sm-12">
 			
--- page-templates/landing-page-title.php
+++ page-templates/landing-page-title.php
@@ -1,24 +1,39 @@
 <?php
 /**
- * Template Name: Landing Page w/ Title
+ * Template Name: Landing Page with Title
  * 
  * @package UFCLAS_UFL_2015
  *
  */
 get_header(); ?>
 
+<?php 
+	if ( has_post_thumbnail() ):
+		$shortcode = sprintf( '[ufclas-landing-page-hero-full headline="%s" image="%d" image_height="half"]%s[/ufclas-landing-page-hero-full]', 
+			get_the_title(),
+			get_post_thumbnail_id(),
+			''
+		);
+		echo do_shortcode( $shortcode );
+	endif;
+?>
 <div id="main" class="container">
-  	<div class="row">
-  		<div class="col-sm-12">
-  			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-  		</div>
-  	</div>
-</div>
-
-<?php while ( have_posts() ) : the_post(); ?>
+    <div class="row">
+        <div class="col-sm-12">
+            <?php 
+				if ( ! has_post_thumbnail() ): 
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				endif;
+			?>
+			
+			<?php while ( have_posts() ) : the_post(); ?>
 	
-	<?php get_template_part( 'template-parts/content', 'landing' ); ?>
+				<?php get_template_part( 'template-parts/content', 'landing' ); ?>
+        
+        	<?php endwhile; // End of the loop. ?>
 
-<?php endwhile; // End of the loop. ?>
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
